ffad0c55685db4bbe87e99f5c6fe3ba55cc668ad
AaronSaada <aaron.saada0@gmail.com>
2024-07-17 11:19:50 +0200

    labor

--- client/projet-crud/src/App.jsx
+++ client/projet-crud/src/App.jsx
@@ -1,8 +1,12 @@
 import { useState } from 'react'
-import './App.css'
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.min.js";
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Reservation from './components/Reservation';
+import UpdateReservation from './components/UpdateRservation';
+import CreateReservation from './components/CreateReservation';
+import SigninComposants from './components/SigninComposants';
+
 
 
 function App() {
@@ -14,7 +18,10 @@ function App() {
                     
             <Routes>
 
-                <Route path='/' element={<Home />} />
+                      <Route path='/' element={<SigninComposants />} />
+                      <Route path='/reservation' element={<Reservation />} />
+                      <Route path='/create' element={<CreateReservation />} />
+                      <Route path='/update/:id' element={<UpdateReservation />} />
               
             </Routes>
 
